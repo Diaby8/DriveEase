21c4a36b978210af47f2367d50e57cc696d602fd
diaby <diaby.diakite@outlook.fr>
2024-12-04 13:25:48 +0000

    supression d'un fichier inutile

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
-import CarsByCategory from '../views/CarsbyCategory.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
-    path: '/cars/category/:type',
-    name: 'cars-by-category',
-    component: CarsByCategory,
-    props: true
   },
   {
     path: '/about',
